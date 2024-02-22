e1bcca7d54b44c9f6cc030099ad9f3c332a485b6
Nicolas <nicolas.peyregne@etu.unicaen.fr>
2024-02-22 10:52:39 +0100

    feat: creation de la connexion

--- AppTrombinoscope/DllbddPersonnels/bddpersonnels.cs
+++ AppTrombinoscope/DllbddPersonnels/bddpersonnels.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DllbddPersonnels
+namespace BddpersonnelContext
 {
     public class bddpersonnels
     {
+        private BddpersonnelDataContext bdd;
+
+        public bddpersonnels(string addrIP, string utilisateur, string mdp, string port)
+        {
+            try
+            {
+                bdd = new BddpersonnelDataContext("User Id=" + utilisateur + ";Password=" + mdp + ";Host=" + addrIP + ";Port=" + port + ";Database=bddrationnel;Persist Security Info=True");
+            } catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
